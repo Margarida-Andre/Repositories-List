5e8bd0fc64886d0813c50e36b6537b69dfb77b26
Margarida-do-Amaral <margaridaandre034@gmail.com>
2022-02-23 17:28:23 +0100

    Uma lista de repositórios aplicando conceitos de componentes, propriedades, estados e imutabilidade do react, foi usado SASS para estilização

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import "./styles/global.scss";
+import { RepositoryList } from "./components/RepositoryList";
+//import { Counter } from "./components/Counter";
+export function App() {
+  return (
+    <>
+      <RepositoryList />
+    </>
+  );
+}
